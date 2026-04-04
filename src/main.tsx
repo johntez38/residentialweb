c6d9b51b0c2449e276b39d0142ef432547df1e5c
John Mureithi <mureithijohn117@gmail.com>
2026-04-04 14:28:03 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import App from './App.tsx' // This must match the file you just checked/created
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
